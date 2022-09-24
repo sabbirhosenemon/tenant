b9b40c3bc5de52f6e55a7458598ae93538689c9b
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-09-24 15:41:48 +0600

    domain chnage

--- .env.js
+++ .env.js
@@ -2,6 +2,6 @@ module.exports = {
   ...process.env,
   MONGODB_URI:
     "mongodb+srv://emonfrom:emonfrom1234@cluster0.xgadn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  DOMAIN: "localhost",
+  DOMAIN: "gscsc.cc",
   PORT: 9000,
 };
